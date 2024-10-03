5339d21df503b1e894a942f6d5e129e9c2b109fb
franco <nazareno@lidernet.com.br>
2024-10-03 11:59:58 -0300

    Trilha inicial

---
+++ src/Desafio/PrincipalLivros.java
@@ -0,0 +1,18 @@
+package Desafio;
+
+public class PrincipalLivros {
+
+    public static void main(String[] args) {
+        Livro livro1 = new Livro();
+        livro1.setTitulo("O Gato de Botas");
+        livro1.setAutor("Charles Perrault");
+
+        Livro livro2 = new Livro();
+        livro2.setTitulo("Faixa Preta");
+        livro2.setAutor("Hirokazu Kanazawa");
+
+        livro1.exibirDetalhes();
+        livro2.exibirDetalhes();
+
+    }
+}
